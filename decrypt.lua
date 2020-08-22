4e321ec1ad812b2da8abf1e0d343ec2023326601
HarvsoDev <68648594+HarvsoDev@users.noreply.github.com>
2020-08-22 21:06:45 +0100

    Add files via upload

---
+++ decrypt.lua
@@ -0,0 +1,37 @@
+local versionid = "" -- Heres where your version id should go.
+
+
+local HttpService = game:GetService("HttpService")
+local link = "http://www.roblox.com/asset/?id=&assetversionid=" ..versionid
+local t = {}
+local content = syn.request(
+    {
+        Url = link,
+        Method = "GET",
+        Headers = {
+            ["User-Agent"] = "Roblox/WinInet";
+            ["Host"] = "www.roblox.com"
+        },
+    }
+)
+for i,v in pairs(content.Headers) do
+    t[i] = v
+end
+
+if tonumber(t["roblox-assetid"]) then
+    setclipboard(t["roblox-assetid"])
+    print(t["roblox-assetid"])
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Successfully copied asset ID to clipboard";
+    Text = "Decryption was successful";
+    Icon = "rbxassetid://54653912";
+    Duration = 5;
+})
+else
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "Failed to grab asset ID";
+    Text = "Decryption failed";
+    Icon = "rbxassetid://16201263";
+    Duration = 5;
+})
+end
